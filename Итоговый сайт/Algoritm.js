12a92a122964c55aa7431b2cdce9324f35f93143
Gwnkjell <gwnkjell@mail.ru>
2021-03-27 17:38:05 +0500

    almost-ready algorithm

--- Итоговый сайт/Algoritm.js
+++ Итоговый сайт/Algoritm.js
@@ -5,17 +5,13 @@ document.addEventListener('DOMContentLoaded', () =>{
 
 	quest.addEventListener('click',()=>{
 		let n = document.getElementById('n').value
-		console.log(n)
 		let data = getData()
 		let text = generate(data)
-		while (n != 0){
-			cont.innerHTML += `<div> ${text} </div>`
-			n --
-		}
+		showMessage(n)
 	})
 })
 function generate(data) {
-	return `Фотокамера делает снимки каждые ${data.t} секунд(-ы), cодержащее ${data.k} оттенков. 
+	return `Фотокамера делает снимки каждые ${data.t} секунд(-ы), cодержащие ${data.k} оттенков. 
 	Размер составляет ${data.w} * ${data.h} пикселей. 
 	Все полученные изображения и коды пикселей внутри одного изображения записываются подряд, никакая дополнительная информация не сохраняется, данные не сжимаются.
 	Сколько Мбайтов нужно выделить для хранения всех изображений, полученных за сутки?`
@@ -26,7 +22,7 @@ function getData() {
 	let pww = Math.floor((Math.random() * 10) % (10 - 6) + 5)
 	let pwh = Math.floor((Math.random() * 10) % (10 - 6) + 5)
 	let k = Math.pow(2,pwp)
-	let t = Math.floor((Math.random() * 10) % (9 - 4) + 4)
+	let t = Math.floor((Math.random() * 10) % (9 - 3) + 3)
 	let w = Math.pow(2,pww) + k
 	let h = Math.pow(2,pwh) + Math.pow(2,t)
 	return {
@@ -34,5 +30,13 @@ function getData() {
 		w: w,
 		h: h,
 		k: k
+	}
+}
+
+function showMessage(k) {
+	let text = generate(getData())
+	cont.innerHTML += `<div> ${text} </br> <input type="number"> <button id="answbtn">Ответить</button> </div>`
+	if (k != 1){
+		showMessage(--k)
 	}
 }
