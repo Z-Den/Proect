2163afeac8700fc5f60e39b1759595b78a8f7dfc
zden <gwnkjell@mail.ru>
2021-04-01 17:27:02 +0500

    html-prjct

---
+++ Итоговый сайт/our prjct/functions.js
@@ -0,0 +1,20 @@
+function isParity (x) {
+	let sum
+	while(x!=0){
+		sum+=x%2
+		x/=2
+	}
+	if(sum%2===0){
+		return 2 * x
+	}else{
+		return 2 * x + 1
+	}
+}
+
+function Duplicate(x) {
+	if(x % 2 === 0){
+		return 2 * x
+	}else{
+		return 2 * x + 1
+	}	
+}
